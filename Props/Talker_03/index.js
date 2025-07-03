e2d171b4858e84c6d5bcdb53c0d63d80270f9771
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2025-07-02 18:14:02 -0600

    One Talker example should be sufficient.

--- Props/Talker_03/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
